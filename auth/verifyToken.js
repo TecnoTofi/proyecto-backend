8cf1936b23e41996c8497aaa31c7976bce323d98
Santiago Topham <santitopham@gmail.com>
2018-10-24 17:44:59 -0300

    Correcciones en autorizacion con JWT

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const secreto = 'keyboard_cat';
 
-verifyToken = (req, res, next) => {
-    if(!req.cookies.access_token){
+const verifyToken = (req, res, next) => {
+    if(!req.headers.token){
         console.log('Token invalido, acceso no autorizado');
         res.status(401).json({message: 'Acceso no autorizado'});
     }
-    const token = req.cookies.access_token;
+    const token = req.headers.token;
     jwt.verify(token, secreto, (error, userData) => {
         if(error){
             console.log(`Error en la verificacion del token : ${error}`);
@@ -16,4 +17,4 @@ verifyToken = (req, res, next) => {
     });
 }
 
-module.exports = { verifyToken };
+module.exports = verifyToken;
--- companies/index.js
+++ companies/index.js
@@ -2,36 +2,14 @@
 const express = require('express');
 //Creamos el router
 const router = express.Router();
-//Incluimos modulo Joi para la validaciond de datos
-// const Joi = require('joi');
-//Incluimos modulo propio con pool de conexion a DB
-// let pool = require('../db/connection');
-const CompanyRoutes = require('./routes');
-// const VerifyToken = require('../auth/verifyToken');
-const jwt = require('jsonwebtoken');
-//Todas las rutas empiezan con /api/companies
-const secreto = 'keyboard_cat';
 
-function verifyToken (req, res, next){
-    console.log(req.headers.token);
-    if(!req.headers.token){
-        console.log('Token invalido, acceso no autorizado');
-        res.status(401).json({message: 'Acceso no autorizado'});
-    }
-    const token = req.headers.token;
-    jwt.verify(token, secreto, (error, userData) => {
-        if(error){
-            console.log(`Error en la verificacion del token : ${error}`);
-            res.status(422).json({message: `Error en la verificacion del token : ${error}`});
-        }
-        req.user = userData;
-        next();
-    });
-}
+const CompanyRoutes = require('./routes');
+const VerifyToken = require('../auth/verifyToken');
+
+//Todas las rutas empiezan con /api/companies
 
 //Ruta para obtener el listado de companyCategory
 router.get('/types', CompanyRoutes.categories);
-router.get('/', CompanyRoutes.companies);
+router.get('/', VerifyToken, CompanyRoutes.companies);
 
 module.exports = router;
-// export default router;
